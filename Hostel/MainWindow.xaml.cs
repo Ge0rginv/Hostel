4f5745733bda5e96531c79cd14ef5f27366c0ea5
Aleksandr <c688xo@mail.ru>
2024-12-03 00:54:52 +0300

    докинул кнопки изменить данные и старт

--- Hostel/MainWindow.xaml.cs
+++ Hostel/MainWindow.xaml.cs
@@ -1,73 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
-namespace Hostel
+namespace HotelBooking
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        private Hotel hotel;
-
         public MainWindow()
         {
             InitializeComponent();
-            //hotel = new Hotel(30); // Инициализация гостиницы
-            //hotel.DisplayRoomStatus(); // Отображение статуса
         }
 
-        private void BookRoom_Click(object sender, RoutedEventArgs e)
+        // Обработчик для кнопки "Изменить данные"
+        private void ChangeDataButton_Click(object sender, RoutedEventArgs e)
         {
-            // Логика для бронирования номера
-            string customerName = CustomerNameTextBox.Text;
-            string roomType = ((ComboBoxItem)RoomTypeComboBox.SelectedItem).Content.ToString();
-            DateTime checkInDate = CheckInDatePicker.SelectedDate ?? DateTime.Now;
-            DateTime checkOutDate = CheckOutDatePicker.SelectedDate ?? DateTime.Now;
+            // Скрываем главные кнопки
+            MainScreen.Visibility = Visibility.Collapsed;
 
-            // Здесь добавьте логику для бронирования
-            // Например, вызов метода для бронирования и обновление статуса
-
-            RoomStatusTextBox.Text += $"Бронирование для {customerName} в {roomType} с {checkInDate.ToShortDateString()} по {checkOutDate.ToShortDateString()}\n";
+            // Показываем элементы для изменения данных
+            DataEntrySection.Visibility = Visibility.Visible;
         }
 
-        private void CheckOut_Click(object sender, RoutedEventArgs e)
+        // Обработчик для кнопки "Старт"
+        private void StartButton_Click(object sender, RoutedEventArgs e)
         {
-            // Логика для оформления выезда
-            // Например, вызов метода для оформления выезда и обновление статуса
+            // Скрываем главные кнопки и элементы для изменения данных
+            MainScreen.Visibility = Visibility.Collapsed;
+            DataEntrySection.Visibility = Visibility.Collapsed;
 
-            RoomStatusTextBox.Text += $"Клиент {CustomerNameTextBox.Text} выехал.\n";
+            // Показываем результаты
+            ResultsSection.Visibility = Visibility.Visible;
+
+            // Пример текста в результатах
+            ResultsTextBox.Text = "Здесь будут отображены доступные номера.";
         }
-
-
-        //private void BookRoom_Click(object sender, RoutedEventArgs e)
-        //{
-        //    hotel.BookRoom(RoomType.Single); // Например, бронируем одноместный номер
-        //    hotel.DisplayRoomStatus();
-        //}
-
-        //private void CheckOut_Click(object sender, RoutedEventArgs e)
-        //{
-        //    hotel.CheckOut(1); // Например, выселение из номера 1
-        //    hotel.DisplayRoomStatus();
-        //}
-
-        //private void RoomStatusTextBox_TextChanged(object sender, TextChangedEventArgs e)
-        //{
-
-        //}
     }
-
 }
